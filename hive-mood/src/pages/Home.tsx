2a93d919ccf825070768cec44556d0c4c34d9bcb
Helena Utzig <helenautzig@gmail.com>
2025-10-22 16:57:57 +0300

    add click on home image

--- hive-mood/src/pages/Home.tsx
+++ hive-mood/src/pages/Home.tsx
@@ -1,40 +1,25 @@
-// import CircularText from '../components/CircularText';
-
-// const Home = () => {
-//   return (
-//     <div className="page">
-//       <h1>Welcome to Hive Mood</h1>
-//       image here, specifc size but responsive
-//       <CircularText
-//         text="HIVE HELSINKI VIBES * THE CODE SCHOOL * "
-//         onHover="speedUp"
-//         spinDuration={20}
-//         className="custom-class"
-//         />
-
-//       <p>Future dashboard or insights will appear here.</p>
-
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import CircularText from '../components/CircularText';
 import image from '../assets/image.jpeg';
 
-const Home = () => {
+
+  const Home: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="page flex flex-col items-center justify-center p-6">
 
       <div className="relative w-full sm:w-3/4 md:w-2/3 lg:w-1/2 max-w-2xl mt-20 mb-10">
+      <button 
+        onClick={() => navigate('/survey')}
+        className="block w-full focus:outline-none">
         <img
           src={image}
           alt="Hive Mood"
           className="w-full h-auto object-cover rounded-2xl shadow-lg"
         />
-
+      </button> 
 
         <div className="absolute -top-20 right-0 translate-x-1/3">
           <CircularText
